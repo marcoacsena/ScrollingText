951a55321ba12831732249ec78730377da1a7368
marcoacsena <marcoacsena@gmail.com>
2020-07-10 18:49:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/android/scrollingtext/MainActivity.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.scrollingtext;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * This app displays a scrollable TextView (a magazine article).
+ * All changes were made to the layout. No code changes.
+ * The code below is part of the Empty Activity template.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvArticle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvArticle = findViewById(R.id.tvArticle);
+        registerForContextMenu(tvArticle);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_context, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.action_editar:
+                Toast.makeText(this, "O item de menu selecionado foi Editar!", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_compartilhar:
+                Toast.makeText(this, "O item de menu selecionado foi Compartilhar!", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.action_excluir:
+                Toast.makeText(this, "O item de menu selecionado foi Excluir!", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+
+        }
+
+
+    }
+}
